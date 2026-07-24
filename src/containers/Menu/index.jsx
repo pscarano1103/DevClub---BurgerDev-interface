ed99fa753646bb72f7ac1ca97988c729fc82cc31
PauloScarano <scaranopaulo@gmail.com>
2026-07-24 17:55:15 -0300

    Implementação de cardápio iniciada

--- src/containers/Menu/index.jsx
+++ src/containers/Menu/index.jsx
@@ -1,23 +1,72 @@
-import { Banner, CategoriesMenu, Container, ProductsContainer } from "./styles";
+import { useEffect, useState } from 'react';
+import {
+  Banner,
+  CategoriesMenu,
+  CategoryButton,
+  Container,
+  ProductsContainer,
+} from './styles';
+import { api } from '../../services/api';
+import { formatPrice } from '../../utils/formatPrice';
+import { CardProduct } from '../../components/CardProduct';
+import { useNavigate } from 'react-router-dom';
 
 export function Menu() {
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    async function loadCategories() {
+      const { data } = await api.get('/categories');
 
-    return (
-        <Container>
-            <Banner>
-                <h1>O MELHOR <br /> HAMBURGER <br /> ESTÁ AQUI <span>Esse cardápio está irresistível</span></h1>
+      const newCategories = [{ id: 0, name: 'Todas' }, ...data];
 
-            </Banner>
+      setCategories(data);
+    }
 
-            <CategoriesMenu>
+    async function loadProducts() {
+      const { data } = await api.get('/products');
 
-            </CategoriesMenu>
+      const newProducts = data.map((product) => ({
+        currencyValue: formatPrice(product.price),
+        ...product,
+      }));
 
-            <ProductsContainer>
+      setProducts(newProducts);
+    }
 
-            </ProductsContainer>
+    loadCategories();
+    loadProducts();
+  }, []);
 
-        </Container>
-    )
+  return (
+    <Container>
+      <Banner>
+        <h1>
+          O MELHOR <br /> HAMBURGER <br /> ESTÁ AQUI{' '}
+          <span>Esse cardápio está irresistível</span>
+        </h1>
+      </Banner>
+
+      <CategoriesMenu>
+        {categories.map((category) => (
+          <CategoryButton
+            key={category.id}
+            onClick={() => {
+              navigate({ pathname: '/cardapio' });
+            }}
+          >
+            {category.name}
+          </CategoryButton>
+        ))}
+      </CategoriesMenu>
+
+      <ProductsContainer>
+        {products.map((product) => (
+          <CardProduct product={product} key={product.id} />
+        ))}
+      </ProductsContainer>
+    </Container>
+  );
 }
--- src/containers/Menu/styles.js
+++ src/containers/Menu/styles.js
@@ -1,45 +1,75 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import BannerHamburger from '../../assets/banner-cardapio.svg';
+import Background from '../../assets/background.svg';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
-    width: 100%;
-    min-height: 100vh;
-    background-color: #f0f0f0;
+  width: 100%;
+  min-height: 100vh;
+  background-color: #f0f0f0;
+  background:
+    linear-gradient(rgba(255, 255, 255, 0.6), rgba(255, 255, 255, 0.6)),
+    url('${Background}');
+  background-size: repeat;
 `;
 
 export const Banner = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 480px;
-    width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 480px;
+  width: 100%;
 
-    position: relative;
+  position: relative;
 
-    background: url('${BannerHamburger}') no-repeat;
-    background-color: #1f1f1f;
-    background-size: cover;
-    background-position: center;
+  background: url('${BannerHamburger}') no-repeat;
+  background-color: #1f1f1f;
+  background-size: cover;
+  background-position: center;
 
-    h1{
-        font-family: 'Road Rage', sans-serif ;
-        font-size: 80px;
-        line-height: 65px;
-        color: #fff;
-        position: absolute;
-        right: 20%;
-        top: 30%;
+  h1 {
+    font-family: 'Road Rage', sans-serif;
+    font-size: 80px;
+    line-height: 65px;
+    color: #fff;
+    position: absolute;
+    right: 20%;
+    top: 30%;
 
-        span{
-            display: block;
-            color: #fff;
-            font-size: 20px;
-            font-weight: 400;
-        }
+    span {
+      display: block;
+      color: #fff;
+      font-size: 20px;
+      font-weight: 400;
     }
+  }
 `;
 
-export const CategoriesMenu = styled.div``;
+export const CategoriesMenu = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+  margin-top: 30px;
+`;
 
-export const ProductsContainer = styled.div``;
+export const CategoryButton = styled(Link)`
+  text-decoration: none;
+  cursor: pointer;
+  background: none;
+  color: #9758a6;
+  font-size: 24px;
+  font-weight: 500;
+  padding-bottom: 5px;
+  line-height: 20px;
+  border-bottom: 3px solid #9758a6;
+`;
 
+export const ProductsContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  padding: 40px;
+  gap: 60px;
+  justify-content: center;
+  max-width: 1280px;
+  margin: 50px auto 0;
+`;
