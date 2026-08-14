2e8df793fcf30d760a3d85b5d5f5077f7429604a
PauloScarano <scaranopaulo@gmail.com>
2026-08-14 14:15:12 -0300

    Montando a tela de categorias pt3: configuramos o menu de categorias para carregar o cardapio ja na categoria desejada, para isso usamos o URLSearchParams do Javascript.

--- src/components/CategoriesCarousel/index.jsx
+++ src/components/CategoriesCarousel/index.jsx
@@ -3,62 +3,69 @@ import { api } from '../../services/api.js';
 
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import { Container, ContainerItens, Title } from './styles.js';
-
+import { CategoryButton, Container, ContainerItens, Title } from './styles.js';
+import { useNavigate } from 'react-router-dom';
 
 export function CategoriesCarousel() {
-    const [categories, setCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        async function loadCategories() {
-            const { data } = await api.get('/categories');
+  useEffect(() => {
+    async function loadCategories() {
+      const { data } = await api.get('/categories');
 
-            setCategories(data)
-            console.log(data);
-        }
+      setCategories(data);
+      console.log(data);
+    }
 
-        loadCategories();
-    }, []);
+    loadCategories();
+  }, []);
 
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 4,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1280 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1280, min: 690 },
+      items: 3,
+    },
+    mobile: {
+      breakpoint: { max: 690, min: 0 },
+      items: 2,
+    },
+  };
 
-    const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 4
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1280 },
-            items: 4
-        },
-        tablet: {
-            breakpoint: { max: 1280, min: 690 },
-            items: 3
-        },
-        mobile: {
-            breakpoint: { max: 690, min: 0 },
-            items: 2
-        }
-    };
+  return (
+    <Container>
+      <Title>Categorias</Title>
 
-
-    return (
-        <Container>
-            <Title>Categorias</Title>
-
-            <Carousel.default
-                responsive={responsive}
-                infinite={true}
-                partialVisbile={false}
-                itemClass='crousel-item'
+      <Carousel.default
+        responsive={responsive}
+        infinite={true}
+        partialVisbile={false}
+        itemClass="crousel-item"
+      >
+        {categories.map((category) => (
+          <ContainerItens key={category.id} imageUrl={category.url}>
+            <CategoryButton
+              onClick={() => {
+                navigate({
+                  pathname: '/cardapio',
+                  search: `?categoria=${category.id}`,
+                });
+              }}
             >
-                {categories.map(category => (
-                    <ContainerItens key={category.id} imageUrl={category.url}>
-                        <p>{category.name}</p>
-                    </ContainerItens>
-                ))}
-
-            </Carousel.default>
-        </Container>
-    )
+              {category.name}
+            </CategoryButton>
+          </ContainerItens>
+        ))}
+      </Carousel.default>
+    </Container>
+  );
 }
--- src/components/CategoriesCarousel/styles.js
+++ src/components/CategoriesCarousel/styles.js
@@ -1,63 +1,67 @@
-import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const Container = styled.div`
+  .crousel-item {
+    padding-right: 40px;
+  }
 
-    .crousel-item{
-        padding-right: 40px ;
-    }
-
-    .react-multiple-carousel__arrow--left {
+  .react-multiple-carousel__arrow--left {
     left: 15px;
     top: 10px;
-    }
+  }
 
-    .react-multiple-carousel__arrow--right{
-        top: 10px;
-    }
+  .react-multiple-carousel__arrow--right {
+    top: 10px;
+  }
 
-    padding-left: 40px;
-`
+  padding-left: 40px;
+`;
 
 export const Title = styled.h2`
-    font-size: 32px;
-    font-weight: 800;
-    color: #9758A6;
-    padding-bottom: 12px;
-    position: relative;
-    text-align: center;
-    margin-bottom: 40px;
-    margin-top: 20px;
+  font-size: 32px;
+  font-weight: 800;
+  color: #9758a6;
+  padding-bottom: 12px;
+  position: relative;
+  text-align: center;
+  margin-bottom: 40px;
+  margin-top: 20px;
 
-    &::after{
-        content: '';
-        position: absolute;
-        bottom: 0;
-        width: 56px;
-        height: 4px;
-        background-color: #9758A6;
-        left: calc(50% - 28px);
-    }
-`
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    width: 56px;
+    height: 4px;
+    background-color: #9758a6;
+    left: calc(50% - 28px);
+  }
+`;
 
 export const ContainerItens = styled.div`
-    background: url('${(props) => props.imageUrl}');
-    background-position: center;
-    background-size: cover;
-    border-radius: 20px;
+  background: url('${(props) => props.imageUrl}');
+  background-position: center;
+  background-size: cover;
+  border-radius: 20px;
 
-    display: flex;
-    align-items: center;
-    padding: 20px 10px;
-    width: 100%;
-    height: 230px;
+  display: flex;
+  align-items: center;
+  padding: 20px 10px;
+  width: 100%;
+  height: 230px;
+`;
+export const CategoryButton = styled(Link)`
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.5);
+  padding: 10px 30px;
+  border-radius: 30px;
+  font-size: 22.5px;
+  margin-top: 50px;
+  text-decoration: none;
+  font-weight: 500;
 
-    p{
-        color: #fff;
-        background-color: rgba(0,0,0,0.5);
-        padding: 10px 30px;
-        border-radius: 30px;
-        font-size: 22.5px;
-        font-weight: bold;
-        margin-top: 50px;
-    }
-`
+  &:hover {
+    background-color: #9758a6;
+  }
+`;
