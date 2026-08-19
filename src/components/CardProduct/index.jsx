159d26e8cb7e153ff44cc93a294d11ee9e1626cc
PauloScarano <scaranopaulo@gmail.com>
2026-08-19 12:24:51 -0300

    Criando a lógica do carrinho de compras

--- src/components/CardProduct/index.jsx
+++ src/components/CardProduct/index.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 import { CardImage, Container } from './styles';
 import { CartButton } from '../CartButton';
+import { useCart } from '../../hooks/CartContext';
 
 export function CardProduct({ product }) {
+  const { putProductInCart } = useCart();
   return (
     <Container>
       <CardImage src={product.url} alt={product.name} />
@@ -11,7 +13,7 @@ export function CardProduct({ product }) {
         <p>{product.name}</p>
         <strong>{product.currencyValue}</strong>
       </div>
-      <CartButton></CartButton>
+      <CartButton onClick={() => putProductInCart(product)}></CartButton>
     </Container>
   );
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   height: 50px;
   background-color: #5c2669;
-  width: 100vw;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
