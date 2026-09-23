c198e77a3ce77cb1d25fd7955e6a8642bc35dc58
PauloScarano <scaranopaulo@gmail.com>
2026-09-23 16:36:16 -0300

    Metodo de pagamento totalmente implementado

--- src/components/Stripe/CheckoutForm/index.jsx
+++ src/components/Stripe/CheckoutForm/index.jsx
@@ -76,7 +76,9 @@ export function CheckoutForm() {
         toast.error('😭 Falha no sistema! Tente novamente');
       }
     } else {
-      toast.error('😭 Falha no sistema! Tente novamente');
+      navigate(
+        `/complete?payment_intent_client_secret=${paymentIntent.client_secret}`,
+      );
     }
 
     setIsLoading(false);
